96b65e42554151eb7b2508bb471983b6fea82df1
Michele De Stefano <--->
2026-04-09 11:52:08 +0200

    Configured the colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,35 @@
+return {
+    {
+        "folke/tokyonight.nvim",
+        opts = {
+            style = "night",
+            transparent = true,
+            styles = {
+                sidebars = "transparent",
+                floats = "transparent",
+            },
+            -- Default colors and highlights are defined into the colors and highlights
+            -- tables into the following file:
+            -- ~/.local/share/nvim/lazy/tokyonight.nvim/extras/lua/tokyonight_night.lua
+            -- We have to modify those fields for making changes. The fields correspond
+            -- to standard VIM names (see :help highlight-groups)
+            on_colors = function(colors)
+                colors.mds_comment = "#B0BECF"
+                colors.mds_line_number = "#98A3B8"
+            end,
+            on_highlights = function(highlights, colors)
+                -- Look at :help highlight-groups for the right variables
+                highlights.NonText = { fg = colors.mds_comment }
+                highlights.EndOfBuffer = { fg = colors.mds_comment }
+                highlights.WinSeparator = { fg = colors.mds_comment, bold = true }
+                highlights.NvimTreeWinSeparator = {
+                    bg = colors.mds_comment,
+                    fg = colors.mds_comment,
+                }
+                highlights.Comment = { fg = colors.mds_comment, italic = true }
+                highlights.LineNrAbove = { fg = colors.mds_line_number }
+                highlights.LineNrBelow = { fg = colors.mds_line_number }
+            end,
+        },
+    },
+}
