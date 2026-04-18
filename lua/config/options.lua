db7eae69bb602f8151d4ab8fa80f68b1743b7a7b
Michele De Stefano <--->
2026-04-19 01:10:17 +0200

    Customized pyright

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,3 +8,24 @@ opt.shiftwidth = 4
 opt.tabstop = 4
 opt.textwidth = 90
 opt.colorcolumn = "91"
+
+-- Python LSP (pyright) configurations
+
+-- Extends pyright's config
+local pyright_cfg = {
+    settings = {
+        python = {
+            analysis = {
+                -- The following avoids warnings when importing symbols that are
+                -- considered as "non-public" (e.g. when a symbol is not listed into the
+                -- __all__ array of exports)
+                diagnosticSeverityOverrides = {
+                    reportPrivateImportUsage = "none",
+                },
+                -- here I could set other options if I need
+            },
+        },
+    },
+}
+
+vim.lsp.config("pyright", pyright_cfg)
