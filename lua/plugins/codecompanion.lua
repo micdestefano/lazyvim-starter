3ecbe236f4495db7b686bde19220de413e8b8c7d
Michele De Stefano <--->
2026-05-16 23:45:26 +0200

    Added codecompanion, configured with ollama

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,52 @@
+return {
+    -- NOTE: At present I've not configured extensions. When confident, add necessary
+    {
+        "olimorris/codecompanion.nvim",
+        version = "*", -- This settings means "always use the latest stable version"
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        -- NOTE: Keymaps beginning with <leader>a are left undefined by LazyVim with the
+        --       exact purpose of being used for AI tools (explained by Gemini). Pay
+        --       atthention that other plugins, like avante, define these combinations, so
+        --       we cannot install also avante together with codecompanion (and I won't do
+        --       that).
+        keys = {
+            {
+                "<leader>ac",
+                "<cmd>CodeCompanionChat Toggle<cr>",
+                desc = "AI Chat (CodeCompanion)",
+                mode = { "n", "v" },
+            },
+            {
+                "<leader>ai",
+                "<cmd>CodeCompanion<cr>",
+                desc = "AI Inline (CodeCompanion)",
+                mode = "n",
+            },
+            {
+                "<leader>ai",
+                ":CodeCompanion<cr>",
+                desc = "AI Inline Refactor (CodeCompanion)",
+                mode = "v",
+            },
+        },
+        opts = {
+            interactions = {
+                chat = {
+                    adapter = {
+                        name = "ollama",
+                        model = "qwen2.5-coder:7b",
+                    },
+                },
+                inline = {
+                    adapter = {
+                        name = "ollama",
+                        model = "qwen2.5-coder:7b",
+                    },
+                },
+            },
+        },
+    },
+}
